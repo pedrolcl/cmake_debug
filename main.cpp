460c9d631678ffa1ec8aabafc6a3a7c590baf894
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2026-08-15 22:45:08 +0200

    actually using Qt6Core5Compat

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <QTextCodec>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QTextCodec *codec = QTextCodec::codecForName("Shift-JIS");
+    QString originalString = "こんにちは世界";
 
-    qDebug() << "Hello!";
+    QByteArray encodedData = codec->fromUnicode(originalString);
+    qDebug() << "Encoded bytes:" << encodedData.toHex();
+
+    QString decodedString = codec->toUnicode(encodedData);
+    qDebug() << "Decoded string:" << decodedString;
 }
