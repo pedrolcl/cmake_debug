7b79f2f9e2db9916d453979de9538e7752251574
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2026-08-16 11:16:43 +0200

    Conditional usage of text codecs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# cmake_bug
-CMake buggy function file(GET_RUNTIME_DEPENDENCIES)
+# cmake debug project
+CMake function file(GET_RUNTIME_DEPENDENCIES) seems to ignore libraries sometimes.
+
+In this project, the CMake option USE_TEXT_CODECS determines if the executable 
+will use the text codecs available in Qt::Core5Compat library. But this library is
+unconditionally linked to the executable, so it is needed by the runtime linker and
+the execution will fail if it is not available at runtime.
+
+When this option USE_TEXT_CODECS is OFF the funtion file(GET_RUNTIME_DEPENDENCIES) omits 
+the Qt::Core5Compat library.
--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 #include <QCoreApplication>
 #include <QDebug>
+
+#if defined(USE_TEXT_CODECS)
 #include <QTextCodec>
+#endif
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QString helloString = "こんにちは世界";
+
+#if defined(USE_TEXT_CODECS)
     QTextCodec *codec = QTextCodec::codecForName("Shift-JIS");
-    QString originalString = "こんにちは世界";
-
-    QByteArray encodedData = codec->fromUnicode(originalString);
+    QByteArray encodedData = codec->fromUnicode(helloString);
     qDebug() << "Encoded bytes:" << encodedData.toHex();
-
     QString decodedString = codec->toUnicode(encodedData);
     qDebug() << "Decoded string:" << decodedString;
+#else
+    qDebug() << helloString;
+#endif
 }
